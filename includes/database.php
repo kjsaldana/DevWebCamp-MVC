601ca7de1dc240d9d2d3dbc5e42d3fabb3a9163b
Kevin J <kjoseph1910@hotmail.com>
2025-12-30 15:55:14 +0100

    docs:clean-database-comments

--- includes/database.php
+++ includes/database.php
@@ -22,18 +22,16 @@
 
 $db = mysqli_init();
 
-// Esto es suficiente para que TiDB acepte la conexión segura.
 $db->ssl_set(NULL, NULL, NULL, NULL, NULL);
 
-// Esta función permite pasar "flags" adicionales, como MYSQLI_CLIENT_SSL
 $db->real_connect(
     $_ENV['DB_HOST'] ?? '',
     $_ENV['DB_USER'] ?? '', 
     $_ENV['DB_PASS'] ?? '', 
     $_ENV['DB_NAME'] ?? '',
     $_ENV['DB_PORT'] ?? 4000,
-    NULL, // Socket (no lo usamos)
-    MYSQLI_CLIENT_SSL // Fuerza el modo seguro
+    NULL,
+    MYSQLI_CLIENT_SSL
 );
 
 if ($db->connect_errno) {
